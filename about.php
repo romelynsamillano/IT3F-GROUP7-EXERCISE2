9bd76063e1f2caab2a31b6aae1f9519e37b3d0d2
romelynsamillano <romesamillano@gmail.com>
2024-09-17 23:52:51 -0700

    Create about.php

---
+++ about.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed|Roboto+Slab" rel="stylesheet">
+    <link rel="stylesheet" href="about.css">
+    <title>About Us</title>
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+        </ul>
+    </nav>
+
+    <main>
+        <h2 class="page-heading">About Us</h2>
+
+        <section>
+            <div class="card">
+                <div class="card-description">
+                    <h3>John Wilfred Tolentino</h3>
+                    <p>
+                        Hi, I'm Wilfred, 31 years old student currently pursuing a Bachelor of Science in Information Technology, now in my third year. I was born on September 28, 1992, and I live at Barangay Poblacion, Muntinlupa City. I'm currently working at Pamahalaang Barangay ng Poblacion as a Cultural Tourism Officer.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="card">
+                <div class="card-description">
+                    <h3>Romelyn Samillano</h3>
+                    <p>
+                        Hi, I'm Romelyn, 20 years old, a 3rd year student at PLMUN currently pursuing a Bachelor of Science in Information Technology. I was born on October 11, 2002. I live at Barangay Cupang, Muntinlupa City. I am currently developing my skills in coding and problem solving.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="card">
+                <div class="card-description">
+                    <h3>Karen Claire Abdon</h3>
+                    <p>
+                        You can call me KC. I am 20 years old and I live at 32c Bunyi Compound, Cupang, Muntinlupa City. I'm currently in my 3rd year of college at PLMun. Because of this program, I am learning a lot about coding and want to explore and learn more.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="card">
+                <div class="card-description">
+                    <h3>Alynna Mae Sanchez</h3>
+                    <p>
+                        Hello, my name is Alynna Mae Sanchez. I'm 20 years old and I live in Bagong Silang, Sucat, Muntinlupa City. I'm currently a student majoring in Information Technology at Pamantasan ng Lungsod ng Muntinlupa. My hobbies are watching K-drama, painting, and playing mobile games. I have a strong passion for technology and enjoy exploring new developments in the field.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="card">
+                <div class="card-description">
+                    <h3>Justine Mae Añonuevo</h3>
+                    <p>
+                        Hi there, I'm Justine Mae Añonuevo. I am from Alabang, Muntinlupa City, and I am twenty years old. I'm currently studying programming and IT systems while earning a Bachelor of Science in Information Technology at Pamantasan ng Lungsod ng Muntinlupa. I have an interest in technology and would like to learn more about the IT industry.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
